7e3523b641e764c2fae1de8ca0522c8e7268052f
HAZberi <mhazuberi@gmail.com>
2021-01-24 21:56:40 -0700

    added svg footer element

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,17 +1,29 @@
 import React from "react";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
+import footerAdornment from "../../assets/Footer Adornment.svg";
 
-const useStyles = makeStyles( theme => ({
-    footer: {
-        backgroundColor: theme.palette.common.orange,
-        width: "100%",
-    }
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.common.orange,
+    width: "100%",
+  },
+  adornment: {
+    width: "20em",
+  },
 }));
 
 const Footer = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return <footer className={classes.footer}>The Footer will go here</footer>
-}
+  return (
+    <footer className={classes.footer}>
+      <img
+        src={footerAdornment}
+        alt="decorative box"
+        className={classes.adornment}
+      />
+    </footer>
+  );
+};
 
 export default Footer;
