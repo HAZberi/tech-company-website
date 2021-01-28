e7d90c03fc13dc79895f5d72baa6ddef495af5c7
HAZberi <mhazuberi@gmail.com>
2021-01-27 17:29:13 -0700

    Hero Block Basic Grid Setup

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Lottie from "react-lottie";
 import {makeStyles} from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import animationData from "../animations/landinganimation/data.js";
 
 const useStyles =  makeStyles(theme => ({
@@ -18,9 +20,29 @@ const LandingPage = () => {
         }
       };
     return (
-        <div>
-            <Lottie options={defaultOptions} height={"100%"} width={"100%"}/>
-        </div>
+        <Grid container direction="column">
+          <Grid item>
+            <Grid container direction="row">
+              <Grid item>
+                <div>Bringing West Coast Technology<br />to the Canadian Prairies</div>
+                <Grid container>
+                  <Grid item>
+                    <Button variant="contained">Get Estimate</Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="contained">
+                      Learn More
+
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Lottie options={defaultOptions} height={"100%"} width={"100%"}/>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
     );
 }
 
