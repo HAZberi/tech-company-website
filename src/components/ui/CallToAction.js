e3de1fd7301cb85f1c263f2f7f972fd8ee487f43
HAZberi <mhazuberi@gmail.com>
2021-01-29 19:01:20 -0700

    call to action complete

--- src/components/ui/CallToAction.js
+++ src/components/ui/CallToAction.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import useMediaQuery from "@material-ui/core/useMediaQuery"
 import ButtonArrow from "./ButtonArrow";
 
 import background from "../../assets/background.jpg";
@@ -14,9 +15,23 @@ const useStyles = makeStyles((theme) => ({
     borderColor: theme.palette.common.orange,
     borderWidth: 3,
     height: "35px",
-    opacity: 0.7,
+    opacity: 0.8,
     marginRight: 0,
     marginLeft: 0,
+  },
+  estimateOutlineButton: {
+    ...theme.typography.learnButton,
+    borderColor: theme.palette.common.hoverBlue,
+    borderWidth: 3,
+    height: "35px",
+    opacity: 0.9,
+    marginRight: 0,
+    marginLeft: 0,
+    marginTop: "15px",
+    "&:hover": {
+        backgroundColor: theme.palette.common.hoverBlue,
+        color: "white",
+      },
   },
   background: {
     backgroundImage: `url(${background})`,
@@ -25,7 +40,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundRepeat: "no-repeat",
     height: "100%",
     width: "100%",
-    [theme.breakpoints.down("md")]: {
+    [theme.breakpoints.down("sm")]: {
       backgroundImage: `url(${mobileBackground})`,
     },
   },
@@ -33,24 +48,33 @@ const useStyles = makeStyles((theme) => ({
 
 const CallToAction = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const smaller = useMediaQuery(theme.breakpoints.down("sm"));
+
 
   return (
-    <Grid container style={{height: "50em"}} alignItems="center">
-      <Grid item style={{position: "absolute"}}>
-        <Grid container direction="column">
+    <Grid container style={{height: smaller ? "85em" : "60em"}} alignItems="start">
+      <Grid item container style={{position: "absolute", marginLeft: smaller ? 0 : "5em", marginTop: smaller ? "5em" : "7.5em", textAlign: smaller ? "center" : "inherit"}}>
+        <Grid sm container direction="column">
           <Grid item>
-            <Typography variant="h2">
+            <Typography variant="h2" gutterBottom>
               Simple Software.
               <br />
               Revolutionary Results.
             </Typography>
-            <Typography variant="subtitle1">
+            <Typography variant="subtitle1" style={{fontWeight: 500, color: "#383838"}} gutterBottom>
               Take advantage of the 21st Century.
             </Typography>
           </Grid>
-          <Grid item>
+          <Grid item style={{marginTop: smaller ? "4em" : 0}}>
             <Button variant="outlined" className={classes.learnOutlineButton}>
               Learn More
+              <ButtonArrow width={25} height={18} fill="black" />
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="outlined" className={classes.estimateOutlineButton}>
+              Get Estimate
               <ButtonArrow width={25} height={18} fill="black" />
             </Button>
           </Grid>
