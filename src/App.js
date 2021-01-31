868cb74dcc543ebc832cbd53f2f3f1ed878a9c82
HAZberi <mhazuberi@gmail.com>
2021-01-30 20:24:11 -0700

    render prop function for Landing page route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./components/Theme.js";
 
 import Header from "./components/ui/Header.js";
 import Footer from "./components/ui/Footer.js";
-import LandingPage from "./components/LandingPage.js"
+import LandingPage from "./components/LandingPage.js";
 const App = () => {
   const [value, setValue] = useState(0);
   const [selected, setSelected] = useState(null);
@@ -13,9 +13,30 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <div>
         <BrowserRouter>
-          <Header value={value} setValue={setValue} selected={selected} setSelected={setSelected}/>
+          <Header
+            value={value}
+            setValue={setValue}
+            selected={selected}
+            setSelected={setSelected}
+          />
           <Switch>
-            <Route exact path="/" component={LandingPage} />
+            {
+              {
+                /* if we need to passdown props to a component within a route tag
+                We should use render prop function to do so. See below using diff */
+              }
+            }
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <LandingPage
+                  {...props}
+                  setValue={setValue}
+                  setSelected={setSelected}
+                />
+              )}
+            />
             <Route
               exact
               path="/services"
@@ -53,7 +74,7 @@ const App = () => {
               component={() => <div>Get your Estimate</div>}
             />
           </Switch>
-          <Footer setValue={setValue} setSelected={setSelected}/>
+          <Footer setValue={setValue} setSelected={setSelected} />
         </BrowserRouter>
       </div>
     </ThemeProvider>
