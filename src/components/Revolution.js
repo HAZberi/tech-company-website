928644625f9d3cb3f96e2c65244b8dff3f055105
HAZberi <mhazuberi@gmail.com>
2021-02-05 20:41:06 -0700

    revolution responsive design final

--- src/components/Revolution.js
+++ src/components/Revolution.js
@@ -8,38 +8,57 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import vision from "../assets/vision.svg";
 
 const useStyles = makeStyles((theme) => ({
-    heading: {
-        maxWidth: "60em",
-        [theme.breakpoints.down("md")]: {
-          maxWidth: "45em",
-        },
-      },
-      arrowContainer: {
-        marginTop: "0.5em",
-      },
-      arrowIcons: {
-        "&:hover": {
-          backgroundColor: "transparent",
-        },
-      },
-      rowContainer: {
-        paddingLeft: "5em",
-        paddingRight: "5em",
-        marginBottom: "10em",
-        [theme.breakpoints.down("sm")]: {
-          paddingLeft: "1.5em",
-          paddingRight: "1.5em",
-        },
-      },
-      itemContainer: {
-        maxWidth: "30em",
+  heading: {
+    maxWidth: "60em",
+    paddingLeft: "5em",
+    paddingRight: "5em",
+    marginBottom: "3em",
+    marginTop: "2em",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "1.5em",
+      paddingRight: "1.5em",
+      textAlign: "center",
+    },
+    [theme.breakpoints.down("md")]: {
+      maxWidth: "45em",
+      marginBottom: "2em",
+    },
+  },
+  arrowContainer: {
+    marginTop: "0.5em",
+  },
+  arrowIcons: {
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+  rowContainer: {
+    paddingLeft: "5em",
+    paddingRight: "5em",
+    marginBottom: "10em",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "1.5em",
+      paddingRight: "1.5em",
+    },
+  },
+  itemContainer: {
+      maxWidth: "50em",
+      marginLeft: "5em",
+    [theme.breakpoints.down("lg")]: {
+        maxWidth: "50%",
         marginLeft: "2.5em",
-        [theme.breakpoints.down("sm")]: {
-          maxWidth: "40rem",
-          marginTop: "2.5rem",
-          marginLeft: 0,
-        },
-      },    
+    },
+    [theme.breakpoints.down("md")]: {
+        maxWidth: "80%",
+        marginLeft: 0,
+        marginTop: "2.5em",
+    },
+    [theme.breakpoints.down("sm")]: {
+      maxWidth: "40rem",
+      marginTop: "2.5rem",
+      marginLeft: 0,
+    },
+  },
 }));
 
 const Revolution = () => {
@@ -47,30 +66,55 @@ const Revolution = () => {
   const theme = useTheme();
   const medium = useMediaQuery(theme.breakpoints.down("md"));
   const smaller = useMediaQuery(theme.breakpoints.down("sm"));
+  const smallest = useMediaQuery(theme.breakpoints.down("xs"));
 
   return (
     <Grid container direction="column">
-      <Grid item>
+      <Grid item className={classes.heading}>
         <Typography variant="h2" gutterBottom>
           The Revolution
         </Typography>
       </Grid>
-      <Grid container direction="row">
-        <Grid item>
-          <img src={vision} alt="Alberta Vision background" />
+      <Grid container direction="row" alignItems="center" justify="center" className={classes.rowContainer}>
+        <Grid item align="center" style={{maxWidth: smallest ? "80%" : "30em"}} md>
+          <img style={{maxWidth: smallest ? "80%" : "30em"}} src={vision} alt="Alberta Vision background" />
         </Grid>
-        <Grid item container direction="column">
-          <Grid item>
+        <Grid item container direction="column" alignItems={smaller ? "center" : "flex-start"} className={classes.itemContainer} lg>
+          <Grid item align={smaller ? "center" : "left"}>
             <Typography variant="h4" gutterBottom>
               Vision
             </Typography>
           </Grid>
-          <Grid item>
-            <Typography variant="body1" paragraph></Typography>
-            <Typography variant="body1" paragraph></Typography>
-            <Typography variant="body1" paragraph></Typography>
-            <Typography variant="body1" paragraph></Typography>
-            <Typography variant="body1" paragraph></Typography>
+          <Grid item align={smaller ? "center" : "left"}>
+            <Typography variant="body1" paragraph>
+              The rise of computers, and subsequently the Internet, has
+              completely altered every aspect of human life. This has increased
+              our comfort, broadened our connections, and reshaped how we view
+              the world.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              What once was confined to huge rooms and teams of engineers now
+              resides in every single one of our hands. Harnessing this
+              unlimited potential by using it to solve problems and better lives
+              is at the heart of everything we do.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              We want to help businesses capitalize on the latest and greatest
+              technology. The best way to predict the future is to be the one
+              building it, and we want to help guide the world into this next
+              chapter of technological expansion, exploration, and innovation.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              By holding ourselves to rigorous standards and pristine quality,
+              we can ensure you have the absolute best tools necessary to thrive
+              in this new frontier.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              We see a future where every individual has personalized software
+              custom tailored to their lifestyle, culture, and interests,
+              helping them overcome life’s obstacles. Each project is a step
+              towards that goal.
+            </Typography>
           </Grid>
         </Grid>
       </Grid>
