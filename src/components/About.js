0abe22cde0be82bb2f05ec25442f104873a91a8d
HAZberi <mhazuberi@gmail.com>
2021-02-07 19:42:59 -0700

    History section scaffolding

--- src/components/About.js
+++ src/components/About.js
@@ -1,50 +1,117 @@
 import React from "react";
-import {makeStyles, useTheme} from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-const useStyles = makeStyles(theme => ({
-    statement: {
-        fontStyle: "italic",
-        fontWeight: 300,
-        fontSize: "1.5rem",
-        maxWidth: "50em",
-        lineHeight: 1.5,
+const useStyles = makeStyles((theme) => ({
+  statement: {
+    fontStyle: "italic",
+    fontWeight: 300,
+    fontSize: "1.5rem",
+    maxWidth: "50em",
+    lineHeight: 1.5,
+  },
+  heading: {
+    paddingLeft: "5em",
+    paddingRight: "5em",
+    marginBottom: "3em",
+    marginTop: "2em",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "1.5em",
+      paddingRight: "1.5em",
+      marginTop: "1em",
+      marginBottom: "1em",
+      textAlign: "center",
     },
-    heading: {
-        paddingLeft: "5em",
-        paddingRight: "5em",
-        marginBottom: "3em",
-        marginTop: "2em",
-        [theme.breakpoints.down("sm")]: {
-          paddingLeft: "1.5em",
-          paddingRight: "1.5em",
-          marginTop: "1em",
-          marginBottom: "1em",
-          textAlign: "center",
-        },
-        [theme.breakpoints.down("md")]: {
-          marginBottom: "1.5em",
-        },
-      },
+    [theme.breakpoints.down("md")]: {
+      marginBottom: "1.5em",
+    },
+  },
 }));
 
 const About = (props) => {
-    const classes = useStyles();
-    const theme = useTheme();
+  const classes = useStyles();
+  const theme = useTheme();
+  const smaller = useMediaQuery(theme.breakpoints.down("sm"));
+  const smallest = useMediaQuery(theme.breakpoints.down("xs"));
+  const larger = useMediaQuery(theme.breakpoints.down("lg"));
+  const medium = useMediaQuery(theme.breakpoints.down("md"));
 
-    return (
-        <Grid container direction="column">
-            <Grid item className={classes.heading}>
-                <Typography variant="h2" gutterBottom>About Us</Typography>
-            </Grid>
-            <Grid item container justify="center">
-                <Typography variant="h4" align="center" className={classes.statement}>
-                    Whether it be person, business to consumer, or an individual to their interests, technology is meant to bring us closer to what we care about in the best way possible. beri.dev will use that principle to provide fast, modern inexpensive and aesthetic software to the Midwest and beyond.
-                </Typography>
-            </Grid>
+  return (
+    <Grid container direction="column">
+      <Grid item className={classes.heading}>
+        <Typography variant="h2" gutterBottom>
+          About Us
+        </Typography>
+      </Grid>
+      <Grid item container justify="center">
+        <Typography variant="h4" align="center" className={classes.statement}>
+          Whether it be person, business to consumer, or an individual to their
+          interests, technology is meant to bring us closer to what we care
+          about in the best way possible. beri.dev will use that principle to
+          provide fast, modern inexpensive and aesthetic software to the Midwest
+          and beyond.
+        </Typography>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justify="center"
+        className={classes.rowContainer}
+      >
+        {/* {medium ? technologyAnimationJSX : null} */}
+        <Grid
+          item
+          container
+          direction="column"
+          alignItems={smaller ? "center" : "flex-start"}
+          style={{
+            marginLeft: 0,
+            marginRight: medium ? 0 : larger ? "2.5em" : "5em",
+          }}
+          className={classes.itemContainer}
+          lg
+        >
+          <Grid item align={smaller ? "center" : "left"}>
+            <Typography variant="h4" gutterBottom>
+              History
+            </Typography>
+          </Grid>
+          <Grid item style={{ textAlign: smallest ? "center" : "justify" }}>
+            <Typography variant="body1" paragraph>
+              We’re the new kid on the block.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Founded in 2019, we’re ready to get our hands on the world’s
+              business problems.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              It all started with one question: Why aren’t all businesses using
+              available technology? There are many different answers to that
+              question: economic barriers, social barriers, educational
+              barriers, and sometimes institutional barriers.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              We aim to be a powerful force in overcoming these obstacles.
+              Recent developments in software engineering and computing power,
+              compounded by the proliferation of smart phones, has opened up
+              infinite worlds of possibility. Things that have always been done
+              by hand can now be done digitally and automatically, and
+              completely new methods of interaction are created daily. Taking
+              full advantage of these advancements is the name of the game.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              All this change can be a lot to keep up with, and that’s where we
+              come in.
+            </Typography>
+          </Grid>
         </Grid>
-    )
-}
+        {/* {medium ? null : technologyAnimationJSX} */}
+      </Grid>
+    </Grid>
+  );
+};
 
-export default About; 
+export default About;
