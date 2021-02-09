a9f79b2363ec4e08fdb8ec5fdc2996e0500a9d52
HAZberi <mhazuberi@gmail.com>
2021-02-08 17:27:16 -0700

    Team section scaffolding

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import CallToAction from "../components/ui/CallToAction.js";
 import history from "../assets/history.svg";
+import founder from "../assets/founder.jpg";
 
 const useStyles = makeStyles((theme) => ({
   statement: {
@@ -53,6 +54,27 @@ const useStyles = makeStyles((theme) => ({
     },
     [theme.breakpoints.down("sm")]: {
       maxWidth: "40rem",
+      marginTop: "2.5rem",
+      marginLeft: 0,
+    },
+  },
+  teamCardContainer: {
+    maxWidth: "30em",
+    marginLeft: "2.5em",
+    marginRight: "2.5em",
+    [theme.breakpoints.down("lg")]: {
+      maxWidth: "30%",
+      marginLeft: "2em",
+      marginRight: "2em",
+    },
+    [theme.breakpoints.down("md")]: {
+      maxWidth: "60%",
+      marginLeft: 0,
+      marginRight: 0,
+      marginTop: "2.5em",
+    },
+    [theme.breakpoints.down("sm")]: {
+      maxWidth: "30rem",
       marginTop: "2.5rem",
       marginLeft: 0,
     },
@@ -148,6 +170,74 @@ const About = (props) => {
           </Grid>
         </Grid>
         {medium ? null : historyJSX}
+      </Grid>
+      <Grid item className={classes.heading} align="center">
+        <Typography variant="h3">Team</Typography>
+      </Grid>
+      <Grid container direction="row" justify="space-around" className={classes.rowContainer}>
+        <Grid item container direction="column" className={classes.teamCardContainer} lg>
+          <Grid item align="center">
+            <img src={founder} style={{maxWidth: "100%"}} alt="founder's headshot" />
+          </Grid>
+          <Grid item style={{marginTop: "1rem"}}>
+            <Typography variant="h4">
+              Hassaan Zuberi
+            </Typography>
+            <Typography variant="caption" gutterBottom>
+              Founder
+            </Typography>
+          </Grid>
+          <Grid item style={{maxWidth: "100%", marginTop: "1.5rem"}}>
+            <Typography variant="body1" paragraph>
+              I taught myself basic coding from a library book in third grade, and ever since then my passion has solely been set on learning — learning about computers, learning mathematics and philosophy, studying design, always just learning.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Now I’m ready to apply everything I’ve learned, and to help others with the intuition I have developed.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item container direction="column" className={classes.teamCardContainer} lg>
+          <Grid item align="center">
+            <img src={founder} style={{maxWidth: "100%"}} alt="founder's headshot" />
+          </Grid>
+          <Grid item style={{marginTop: "1rem"}}>
+            <Typography variant="h4">
+              Hassaan Zuberi
+            </Typography>
+            <Typography variant="caption" gutterBottom>
+              Founder
+            </Typography>
+          </Grid>
+          <Grid item style={{maxWidth: "100%", marginTop: "1.5rem"}}>
+            <Typography variant="body1" paragraph>
+              I taught myself basic coding from a library book in third grade, and ever since then my passion has solely been set on learning — learning about computers, learning mathematics and philosophy, studying design, always just learning.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Now I’m ready to apply everything I’ve learned, and to help others with the intuition I have developed.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item container direction="column" className={classes.teamCardContainer} lg>
+          <Grid item align="center">
+            <img src={founder} style={{maxWidth: "100%"}} alt="founder's headshot" />
+          </Grid>
+          <Grid item style={{marginTop: "1rem"}}>
+            <Typography variant="h4">
+              Hassaan Zuberi
+            </Typography>
+            <Typography variant="caption" gutterBottom>
+              Founder
+            </Typography>
+          </Grid>
+          <Grid item style={{maxWidth: "100%", marginTop: "1.5rem"}}>
+            <Typography variant="body1" paragraph>
+              I taught myself basic coding from a library book in third grade, and ever since then my passion has solely been set on learning — learning about computers, learning mathematics and philosophy, studying design, always just learning.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Now I’m ready to apply everything I’ve learned, and to help others with the intuition I have developed.
+            </Typography>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item>
         {/* Call to Action Block */}
