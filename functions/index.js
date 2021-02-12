d33b1e6026476d7fec1b42e3cbee46aaf724d41f
HAZberi <mhazuberi@gmail.com>
2021-02-12 11:28:22 -0700

    firebase function connected

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const functions = require("firebase-functions");
+
+const config = functions.config();
+
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
