e9c597716d4bac94966aa2d88c7e108dababa7f0
HAZberi <mhazuberi@gmail.com>
2021-02-13 00:13:42 -0700

    firbase and nodemailer setup

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,30 @@
 const functions = require("firebase-functions");
-
-const config = functions.config();
-
 const admin = require("firebase-admin");
-
+const nodemailer = require("nodemailer");
+const config = functions.config();
 admin.initializeApp();
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: config.user.email,
+    pass: config.user.password,
+  },
+});
+
+const mailOptions = {
+  from: "Beri Technologies",
+  to: "mhazuberi@gmail.com",
+  subject: "Testing NodEmailer Setup",
+  text: "Test Successful",
+};
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendMail = functions.https.onRequest((request, response) => {
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) response.send(error);
+    else response.send("Message sent successfully");
+  });
+});
