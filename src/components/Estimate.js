f9a24c4264abd80ba47622ddd27824fece710a2d
HAZberi <mhazuberi@gmail.com>
2021-02-18 10:49:49 -0700

    advanced option & service type selection final

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -348,7 +348,7 @@ const Estimate = (props) => {
   const medium = useMediaQuery(theme.breakpoints.down("md"));
   const smallest = useMediaQuery(theme.breakpoints.down("xs"));
 
-  const [questions, setQuestions] = useState(softwareQuestions);
+  const [questions, setQuestions] = useState(defaultQuestions);
 
   const defaultOptions = {
     loop: true,
@@ -398,9 +398,7 @@ const Estimate = (props) => {
     //creating a deep copy of state Questions to keep the state immutable
     const updatedQuestions = cloneDeep(questions);
     //filtering out the currently active question
-    const activeQuestion = updatedQuestions.filter(
-      (question) => question.active
-    );
+    let activeQuestion = updatedQuestions.filter((question) => question.active);
     //Getting index of active question
     const activeQuestionIndex = activeQuestion[0].id - 1;
     //Getting next question index
@@ -417,6 +415,16 @@ const Estimate = (props) => {
     };
     //Finally Update the question structure in state
     setQuestions(updatedQuestions);
+
+    //if we need to restrict user to select only one type of service
+    //its kind of a reset function for question
+    //Without this block of code user can still go back n forth between questions even after going back to first question
+    // activeQuestion = updatedQuestions.filter(
+    //   (question) => question.active
+    // );
+    // if(activeQuestion[0].title === 'Which service are you interested in?'){
+    //   setQuestions(defaultQuestions);
+    // }
   };
 
   const previousQuestionDisabled = () => {
@@ -441,24 +449,43 @@ const Estimate = (props) => {
     const selectedOption =
       updatedQuestions[activeQuestionIndex].options[id - 1];
     //Getting the previous selected option from currently active question and filter for any other options seleted
-    const previouslySelectedOption = activeQuestion[0].options.filter(option => option.selected);
+    const previouslySelectedOption = activeQuestion[0].options.filter(
+      (option) => option.selected
+    );
     //switch over active question subtitle to find whether single or multiple questions needed to be selected.
-    switch(activeQuestion[0].subtitle){
+    switch (activeQuestion[0].subtitle) {
       //The case where only one option can be seleted at a time
-      case 'Select one.': 
+      case "Select one.":
         //Check if there is any already or previously selected option exist
         //if condition is true => toggle the previously selected option
-        if(previouslySelectedOption[0]) previouslySelectedOption[0].selected = !previouslySelectedOption[0].selected;
+        if (previouslySelectedOption[0])
+          previouslySelectedOption[0].selected = !previouslySelectedOption[0]
+            .selected;
         //if there is no previously selected option then Toggle the options currently selected property
         selectedOption.selected = !selectedOption.selected;
         break;
-      default: 
+      default:
         //In all other cases user can select multiple options, so toggle selection
         selectedOption.selected = !selectedOption.selected;
         break;
     }
-    //Finally update the question structure in state
-    setQuestions(updatedQuestions);
+    //switch over selected option title to find which set of questions should be shown to the user
+    switch (selectedOption.title) {
+      //if selected option title matches the swtich case => question state is updated to show relevant questions
+      case "Software Development":
+        setQuestions(softwareQuestions);
+        break;
+      case "App Development":
+        setQuestions(softwareQuestions);
+        break;
+      case "Website Development":
+        setQuestions(websiteQuestions);
+        break;
+      default:
+        //if selected option title doesnt match any switch case then we will simply update the question
+        setQuestions(updatedQuestions);
+        break;
+    }
   };
 
   return (
