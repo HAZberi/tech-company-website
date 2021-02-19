a62aecd2214d395ef4ddabab24a58d1a0fb790c4
HAZberi <mhazuberi@gmail.com>
2021-02-19 13:44:11 -0700

    platform grammer script added

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -890,7 +890,31 @@ const Estimate = (props) => {
                     </Grid>
                     <Grid item>
                       <Typography variant="body1">
-                        You want {service}
+                        You want {service} {platforms.length > 0 ? `for ${
+                              //if only web application is selected...
+                              platforms.indexOf("Web Application") > -1 &&
+                              platforms.length === 1
+                                ? //then finish sentence here
+                                  "a Web Application."
+                                : //otherwise, if web application and another platform is selected...
+                                platforms.indexOf("Web Application") > -1 &&
+                                  platforms.length === 2
+                                ? //then finish the sentence here
+                                  `a Web Application and an ${platforms[1]}.`
+                                : //otherwise, if only one platform is selected which isn't web application...
+                                platforms.length === 1
+                                ? //then finish the sentence here
+                                  `an ${platforms[0]}`
+                                : //otherwise, if other two options are selected...
+                                platforms.length === 2
+                                ? //then finish the sentence here
+                                  "an iOS Application and an Android Application."
+                                : //otherwise if all three are selected...
+                                platforms.length === 3
+                                ? //then finish the sentence here
+                                  "a Web Application, an iOS Application, and an Android Application."
+                                : null
+                            }`: null}
                       </Typography>
                     </Grid>
                   </Grid>
