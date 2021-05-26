9a9e3826670460f730855cfe24c428d384e6c5ca
Hassaan Zuberi <mhazuberi@gmail.com>
2021-05-25 23:10:18 -0600

    refined directory structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
-import theme from "./components/Theme.js";
+import theme from "./components/ui/Theme.js";
 import ScrollToTop from "./scripts/ScrollToTop.js";
 
 import Header from "./components/ui/Header.js";
